f8cace96f720a5d9406616b13b1d02cbe3f5b70b
Vaibhavdesai99 <vaibhavdesai510@gmail.com>
2023-07-28 12:53:37 +0530

    Fat Arrow And Objects

--- FirstProject.js
+++ FirstProject.js
@@ -1,2 +1,21 @@
 
-console.log('Hello World')
+// console.log('Hello World')
+
+// Fat Arrow : - 
+
+const Student ={
+    name :'vaibhav',
+    age : 24, 
+
+     greet : function (){
+        console.log(` Hi My name is  ${this.name}`)                       // Hi My name is vaibhav
+     },
+
+
+     greet2 :() => {
+        console.log(` Hi My name is  ${this.name}`)                       //Show undefined here caz fat arrow dont have this keyword it take it from its parent .
+     }
+}
+
+Student.greet()
+Student.greet2()
