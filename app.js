e0693725ee8736243a9a7eec36f79209f0a3d54b
Vaibhavdesai99 <vaibhavdesai510@gmail.com>
2023-07-29 18:10:53 +0530

    Request / Response

--- app.js
+++ app.js
@@ -2,7 +2,28 @@
 let http = require('http')
 
 const server = http.createServer((req,res)=>{
-    console.log('vaibhav')
-})
+    res.setHeader('Content-type' ,'text/html')
 
-server.listen(4000)
+    // parse the url
+    const url = req.url
+ 
+
+    if (url === '/home') {
+        res.write('<html>');
+        res.write('<head><title>Home</title></head>');
+        res.write('<body><h1>Welcome home</h1></body>');
+        res.write('</html>');
+    } else if (url === '/about') {
+        res.write('<html>');
+        res.write('<head><title>About Us</title></head>');
+        res.write('<body><h1>Welcome to About Us page</h1></body>');
+        res.write('</html>');
+    } else if (url === '/node') {
+        res.write('<html>');
+        res.write('<head><title>Node.js Project</title></head>');
+        res.write('<body><h1>Welcome to my Node.js project</h1></body>');
+        res.write('</html>');
+
+}})
+
+server.listen(3000)
